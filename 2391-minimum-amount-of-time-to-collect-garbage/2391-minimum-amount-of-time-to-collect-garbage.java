bc51a3824417f2994c9bb0de7d439fe9527c9c98
hossamabdullah <hossamabdullah@users.noreply.github.com>
2022-10-13 00:17:32 +0200

    Time: 40 ms (72.82%), Space: 96.4 MB (39.07%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
@@ -16,9 +16,9 @@ class Solution {
             c += garbage[i].length();
             if(i==0)
                 break;
-            if(garbage[i].indexOf('P') != -1 && !p) { c+=accumlatedTravel[i-1]; p=true;}
-            if(garbage[i].indexOf('M') != -1 && !m) { c+=accumlatedTravel[i-1]; m=true;}
-            if(garbage[i].indexOf('G') != -1 && !g) { c+=accumlatedTravel[i-1]; g=true;}
+            if(garbage[i].contains("P") && !p) { c+=accumlatedTravel[i-1]; p=true;}
+            if(garbage[i].contains("M") && !m) { c+=accumlatedTravel[i-1]; m=true;}
+            if(garbage[i].contains("G") && !g) { c+=accumlatedTravel[i-1]; g=true;}
             // if(p!=0 && m!=0 && g!=0)
                 // break;
         }

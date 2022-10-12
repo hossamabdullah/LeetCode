fcf29d56bab1d54f3cdfe183e85352528939a3d7
hossamabdullah <hossamabdullah@users.noreply.github.com>
2022-10-13 00:20:40 +0200

    Time: 4 ms (99.83%), Space: 67.7 MB (95.14%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
@@ -22,6 +22,8 @@ class Solution {
             if(currentGarbage.contains("P") && !p) { c+=accumlatedTravel[i-1]; p=true;}
             if(currentGarbage.contains("M") && !m) { c+=accumlatedTravel[i-1]; m=true;}
             if(currentGarbage.contains("G") && !g) { c+=accumlatedTravel[i-1]; g=true;}
+            // if(p!=0 && m!=0 && g!=0)
+                // break;
         }
         
         return c;

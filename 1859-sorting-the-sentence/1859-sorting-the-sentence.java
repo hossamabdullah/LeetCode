5d20768bf2ff069e6101de16c2dec349cf789a57
hossamabdullah <hossamabdullah@users.noreply.github.com>
2022-10-16 13:04:32 +0200

    Time: 1 ms (94.19%), Space: 42.1 MB (43.75%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -5,8 +5,12 @@ class Solution {
         
         for(int i=0; i<words.length; i++){
             int lastIndexOfCurrentWord = words[i].length() - 1;
-            int index = Integer.parseInt(""+words[i].charAt(lastIndexOfCurrentWord)) -1;
-            sorted[index] = words[i].substring(0, lastIndexOfCurrentWord);  
+            int index = words[i].charAt(lastIndexOfCurrentWord) - '0';
+            sorted[index-1] = words[i].substring(0, lastIndexOfCurrentWord); 
+            
+            
+            // int correctIndex = words[i].charAt(words[i].length()-1)-'0';
+            // sorted[correctIndex-1] = words[i].substring(0, words[i].length()-1);
         }
         
         String result = String.join(" ", sorted);

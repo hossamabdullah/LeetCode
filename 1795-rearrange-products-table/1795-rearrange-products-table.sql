965a32c743c2a3340ac136c40401bb2977cec693
hossamabdullah <hossamabdullah@users.noreply.github.com>
2022-10-17 00:08:09 +0200

    Time: 963 ms (18.52%), Space: 0B (100.00%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.sql
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-select product_id,  if(store1 is not null, 'store1', null) as store, sum(store1) as price from Products where store1 is not null group by product_id, store
+select product_id,  'store1' as store, store1 as price from Products where store1 is not null group by product_id, store
 union
-select product_id,  if(store2 is not null, 'store2', null) as store, sum(store2) as price from Products where store2 is not null group by product_id, store
+select product_id,  'store2' as store, store2 as price from Products where store2 is not null group by product_id, store
 union
-select product_id,  if(store3 is not null, 'store3', null) as store, sum(store3) as price from Products where store3 is not null group by product_id, store;
+select product_id,  'store3' as store, store3 as price from Products where store3 is not null group by product_id, store

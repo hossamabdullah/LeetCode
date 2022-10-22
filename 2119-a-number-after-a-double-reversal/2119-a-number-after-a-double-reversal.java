95797b86f08a131d7dea50420c436cdb94481697
hossamabdullah <hossamabdullah@users.noreply.github.com>
2022-10-22 14:15:41 +0200

    Time: 1 ms (32.14%), Space: 41.2 MB (30.81%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isSameAfterReversals(int num) {
         String x = num+"";
-        if(x.length() > 1 && x.charAt(x.length()-1) == '0')
+        if(num != 0 && x.charAt(x.length()-1) == '0')
             return false;
         else
             return true;

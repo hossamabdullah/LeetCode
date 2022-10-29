359e2793198880dbc9f49059cce26e95f36ed050
hossamabdullah <hossamabdullah@users.noreply.github.com>
2022-10-29 20:28:40 +0200

    Time: 44 ms (6.98%), Space: 56.1 MB (79.77%) - LeetHub

---
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -0,0 +1,28 @@
+class Solution {
+    private static final String TYPE_RULE = "type";
+    private static final int TYPE_INDEX = 0;
+    private static final String COLOR_RULE = "color";
+    private static final int COLOR_INDEX = 1;
+    private static final String NAME_RULE = "name";
+    private static final int NAME_INDEX = 2;
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int count = 0;
+        
+        for(int i=0; i<items.size(); i++){
+            if(ruleKey.equals(TYPE_RULE) 
+                     && items.get(i).get(TYPE_INDEX).equals(ruleValue)){
+                System.out.println(i+"--- type");
+                count++;
+            }else if(ruleKey.equals(COLOR_RULE) 
+                     && items.get(i).get(COLOR_INDEX).equals(ruleValue)){
+                System.out.println(i+"--- color");
+                count++;
+            }else if(ruleKey.equals(NAME_RULE)
+                     && items.get(i).get(NAME_INDEX).equals(ruleValue)){
+                System.out.println(i+"--- name");
+                count++;
+            }
+        }
+        return count;
+    }
+}

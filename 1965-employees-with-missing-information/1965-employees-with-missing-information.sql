f4ea4d0a17d8d9a91fb997823df06adf518010bf
hossamabdullah <hossamabdullah@users.noreply.github.com>
2022-11-02 07:33:54 +0200

    Time: 956 ms (23.18%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,10 +1,9 @@
 # Write your MySQL query statement below
-select employee_id from
 (select s.employee_id as employee_id from Employees as e
 RIGHT JOIN  Salaries as s ON e.employee_id = s.employee_id
 WHERE e.name is null
 UNION
 select e.employee_id as  employee_id from Employees as e
 LEFT JOIN  Salaries as s ON e.employee_id = s.employee_id
-WHERE s.salary is null) as emp_data
+WHERE s.salary is null)
 order by employee_id asc;

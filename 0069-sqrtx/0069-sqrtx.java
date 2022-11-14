eefd32fa9ee232ac1f69d66c84d4dafc90abbdc1
hossamabdullah <hossamabdullah@users.noreply.github.com>
2022-11-15 01:08:09 +0200

    Time: 2 ms (84.26%), Space: 42 MB (6.73%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x <= 1)
+            return x;
+        
+        int start = 1;
+        int end = x/2;
+        
+        while(end > start){
+            int mid = start + ((end-start)/2) + 1;
+            
+            int current = x/mid;
+            if(current == mid)
+                return mid;
+            if(current>mid)
+                start = mid;
+            else
+                end = mid-1;
+        }
+        
+        return start;
+    }
+}
